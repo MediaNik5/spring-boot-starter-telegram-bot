535d40aa091aedb1d8bcad53cff2a3b7d38632fa
MediaNik5 <stafilopok@mail.ru>
2022-06-30 18:04:46 +0300

    Remove redundant dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.reflections:reflections:0.10.2")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
